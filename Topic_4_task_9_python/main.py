08ef0feb9ec04fe45a982f6d0df0d069dc8e69e9
Darya Zgirskaya <129535397+Antrsss@users.noreply.github.com>
2025-05-28 21:31:18 +0300

    Added time in python for topic 3 task 9, topic 4 task 9

--- Topic_4_task_9_python/main.py
+++ Topic_4_task_9_python/main.py
@@ -1,6 +1,41 @@
 from solution_1 import solution_1
 from solution_2 import input_matrix, solution_2
+import time
+import numpy as np
 
-matrix = input_matrix()
-print(f"Ответ (solution_1): {solution_1(matrix)}")
-print(f"Ответ (solution_2): {solution_2(matrix)}")
+size = 20
+
+sparse_matrix = np.zeros((size, size), dtype=int)
+for i in range(size - 1):
+    sparse_matrix[i, i + 1] = 1
+    sparse_matrix[i + 1, i] = 1
+
+dense_matrix = np.zeros((size, size), dtype=int)
+for i in range(size):
+    for j in range(size):
+        if i != j:
+            dense_matrix[i, j] = 1
+
+print("Разряженная матрица:")
+start_time = time.perf_counter()
+print(f"Ответ (solution_1): {solution_1(sparse_matrix)}")
+end_time = time.perf_counter()
+print(f"Время выполнения: {end_time - start_time:.6f} секунд")
+
+start_time = time.perf_counter()
+print(f"Ответ (solution_2): {solution_2(sparse_matrix)}")
+end_time = time.perf_counter()
+print(f"Время выполнения: {end_time - start_time:.6f} секунд")
+
+
+
+print("Плотная матрица:")
+start_time = time.perf_counter()
+print(f"Ответ (solution_1): {solution_1(sparse_matrix)}")
+end_time = time.perf_counter()
+print(f"Время выполнения: {end_time - start_time:.6f} секунд")
+
+start_time = time.perf_counter()
+print(f"Ответ (solution_2): {solution_2(sparse_matrix)}")
+end_time = time.perf_counter()
+print(f"Время выполнения: {end_time - start_time:.6f} секунд")
